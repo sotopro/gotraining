91ca4a91ba7f47e141ea1195e773a82a3774cdb3
Daniel Soto <dasg.1993@gmail.com>
2020-06-15 17:52:44 -0500

    [fix] - packages

--- packages/utils/reverse.go
+++ packages/utils/reverse.go
@@ -2,7 +2,7 @@
 // encoded strings, beyond what is provided in the standard "strings" package.
 package utils
 
-// ReverseRunes : ReverseRunes string
+// ReverseRunes returns its argument string reversed rune-wise left to right.
 func ReverseRunes(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
